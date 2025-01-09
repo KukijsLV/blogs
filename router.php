a2e75172cd1ec5aebb88fec6cd5990e5430d8012
Skolnieks <skolnieks.pjvt@pjvt.local>
2025-01-09 15:11:33 +0200

    s

--- router.php
+++ router.php
@@ -1,2 +1,11 @@
 <?php
 dd($_SERVER["REQUEST_ URI"]);
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
+if($uri == "/"){
+    require "controllers/index.php";
+}else if($uri == "about"){
+    require "controllers/story.php";
+}else if ($uri == "cateogries"){
+    require "controllers/cateogries.php";
+} ?>
+
